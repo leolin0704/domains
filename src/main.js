22d6284bb1a61fa35798347491f5304217930025
Li Lin <84984903@qq.com>
2021-05-10 00:24:39 +0800

    refactor

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ console.log( path.join(__dirname, 'main'))
 
 // first plugin
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'main'),
+  root: path.join(__dirname, 'static'),
   prefix: '/public/'
 })
 
@@ -19,6 +19,11 @@ fastify.get('/status', async (request, reply) => {
   reply.type('application/json').code(200)
   console.log(request)
   return { hello: 'world' }
+})
+
+fastify.get('/index', async (request, reply) => {
+  reply.headers['cookie'] = 'test=' + new Date().toISOString() + '; HttpOnly'
+  return reply.sendFile('index.html')
 })
 
 fastify.listen(3000, (err, address) => {
